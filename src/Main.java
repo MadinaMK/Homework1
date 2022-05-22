a8663c8f8302fa9bd44e0e89429e1f89e8f2dfcc
Madina Karazhanova <karazhanovam@mail.ru>
2022-05-22 22:44:19 +0600

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int currentAmount = 2_000_000_000;
+        int transferAmount = 500_000_000;
+        int totalAmount = currentAmount + transferAmount;
+        System.out.println(totalAmount);
+    }
+}
